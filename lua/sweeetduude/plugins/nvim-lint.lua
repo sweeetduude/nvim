0a6bbefe038774df2a6adf9d6f897c29f79e2d72
sweeetduude <waytolegend1@gmail.com>
2024-01-03 16:04:49 +0100

    update

--- lua/sweeetduude/plugins/nvim-lint.lua
+++ lua/sweeetduude/plugins/nvim-lint.lua
@@ -8,7 +8,7 @@ return {
         lint.linters_by_ft = {
             javascript = { "eslint_d" },
             typescript = { "eslint_d" },
-            vue = { "eslint_d" },
+            --vue = { "eslint_d" },
             --python = { "pylint" },
         }
 
--- lua/sweeetduude/remap.lua
+++ lua/sweeetduude/remap.lua
@@ -1,11 +1,5 @@
 vim.g.mapleader = " "
 
-for _, mode in pairs({ 'n', 'i', 'v', 'x' }) do
-    for _, key in pairs({ '<Up>', '<Down>', '<Left>', '<Right>', '<Home>', '<End>' }) do
-        vim.keymap.set(mode, key, '<nop>')
-    end
-end
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -13,3 +7,9 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "Q", "<nop>")
+
+for _, mode in pairs({ 'n', 'i', 'v', 'x' }) do
+    for _, key in pairs({ '<Up>', '<Down>', '<Left>', '<Right>', '<Home>', '<End>' }) do
+        vim.keymap.set(mode, key, '<nop>')
+    end
+end
