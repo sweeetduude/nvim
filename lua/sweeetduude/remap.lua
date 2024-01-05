894246d2108653972a228a77e32d4cdea95212df
sweeetduude <waytolegend1@gmail.com>
2024-01-05 20:42:21 +0100

    update

--- lua/sweeetduude/remap.lua
+++ lua/sweeetduude/remap.lua
@@ -1,27 +1,34 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "move line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "move line up" })
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "scroll down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "scroll up" })
+
+vim.keymap.set("n", "+", "$", { desc = "end of line" })
+vim.keymap.set("n", "<leader>de", "d$", { desc = "delete to end of line" })
+
+vim.keymap.set("n", "<leader>db", "0f=hc^", { desc = "chage before =" })
+vim.keymap.set("n", "<leader>da", "0f=lc$", { desc = "chage after =" })
 
 vim.keymap.set("n", "Q", "<nop>")
-
-vim.keymap.set("n", "+", "$")
-
-
-vim.keymap.set("n", "<leader>de", "d$")
-
-
 for _, mode in pairs({ 'n', 'i', 'v', 'x' }) do
-    for _, key in pairs({ '<Up>', '<Down>', '<Left>', '<Right>', '<Home>', '<End>' }) do
+    for _, key in pairs({ '<Up>', '<Down>', '<Left>', '<Right>', '<Home>', '<End>', '<Del>' }) do
         vim.keymap.set(mode, key, '<nop>')
     end
 end
 
+-- add C-c to add to clipboard
+vim.keymap.set('v', "<C-c>", '"+y')
+
+-- add C-x to cut to clipboard
+vim.keymap.set('v', "<C-x>", '"+d')
+
+-- add C-v to paste from clipboard
+vim.keymap.set('i', "<C-v>", '<esc>"+p')
 
 
--- change before/after first =
-vim.keymap.set("n", "<leader>db", "0f=hc^", { desc = "chage before =" })
-vim.keymap.set("n", "<leader>da", "0f=lc$", { desc = "chage after =" })
+
+vim.keymap.set('v', '<leader>gpt', 'yGoInstructions: <esc>OCode: <esc>p<esc>kVG"+d',
+    { desc = "copy to chat-GPT template" })
