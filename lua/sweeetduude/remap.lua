cf3b7117b483ebadd49a1f45a997ec816690caae
sweeetduude <waytolegend1@gmail.com>
2024-01-06 20:10:07 +0100

    update

--- lua/sweeetduude/remap.lua
+++ lua/sweeetduude/remap.lua
@@ -8,13 +8,21 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "scroll up" })
 
 vim.keymap.set("n", "+", "$", { desc = "end of line" })
 vim.keymap.set("n", "<leader>de", "d$", { desc = "delete to end of line" })
-
 vim.keymap.set("n", "<leader>db", "0f=hc^", { desc = "chage before =" })
 vim.keymap.set("n", "<leader>da", "0f=lc$", { desc = "chage after =" })
 
+vim.keymap.set("v", "<leader>a(", "c()<esc>P", { desc = "put selection in parens" })
+vim.keymap.set("v", "<leader>a[", "c[]<esc>P", { desc = "put selection in square brackets" })
+vim.keymap.set("v", "<leader>a{", "c{}<esc>P", { desc = 'put selection in squiggly brackets' })
+vim.keymap.set("v", "<leader>a<", "c<><esc>P", { desc = "put selection in angle brackets" })
+vim.keymap.set("v", "<leader>a'", "c''<esc>P", { desc = "put selection in single quotes" })
+vim.keymap.set("v", "<leader>a\"", "c\"\"<esc>P", { desc = "put selection in double quotes" })
+vim.keymap.set("v", "<leader>a`", "c``<esc>P", { desc = "put selection in backticks" })
+
+
 vim.keymap.set("n", "Q", "<nop>")
 for _, mode in pairs({ 'n', 'i', 'v', 'x' }) do
-    for _, key in pairs({ '<Up>', '<Down>', '<Left>', '<Right>', '<Home>', '<End>', '<Del>' }) do
+    for _, key in pairs({ '<Up>', '<Down>', '<Left>', '<Right>', '<Home>', '<End>' }) do
         vim.keymap.set(mode, key, '<nop>')
     end
 end
@@ -30,5 +38,5 @@ vim.keymap.set('i', "<C-v>", '<esc>"+p')
 
 
 
-vim.keymap.set('v', '<leader>gpt', 'yGoInstructions: <esc>OCode: <esc>p<esc>kVG"+d',
+vim.keymap.set('v', '<leader>gpt', 'yGoInstructions:<esc>OCode:<esc>p<esc>kVG"+d',
     { desc = "copy to chat-GPT template" })
